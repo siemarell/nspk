c4787fd7ccafbf57b5163c1e1cd06636fc9ac128
siem <siemarell@gmail.com>
2016-10-21 16:08:00 +0300

    loader + csv source

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ import json
 import requests
 
 from dataloader import DataLoader
+from dataloader import CsvSource
 
 if __name__ == '__main__':
     dataLoader = DataLoader()
     dataLoader.load_metadata()
+    dataLoader.load_data(CsvSource())
+
