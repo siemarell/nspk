8fcec871ada60def37ad92599bcdea6285952a00
siem <siemarell@gmail.com>
2016-10-24 11:00:39 +0300

    pg_source ready

--- data_sources.py
+++ data_sources.py
@@ -1,27 +1,28 @@
 import json
 import csv
 import psycopg2
+import datetime
 from os import listdir
 from config import config
 
 pg_types = {
-    "1043": {
+    1043: {
         "name": "varchar",
         "vc_type": "string"
     },
-    "25": {
+    25: {
         "name": "text",
         "vc_type": "string"
     },
-    "701": {
+    701: {
         "name": "float8",
         "vc_type": "float"
     },
-    "23": {
+    23: {
         "name": "int4",
         "vc_type": "integer"
     },
-    "1082": {
+    1082: {
         "name": "date",
         "vc_type": "date"
     }
@@ -43,7 +44,7 @@ class CsvSource:
         try:
             file = open('%s/data/%s.csv' % (self.path, table))
             #reader = csv.reader(file)
-            return SmartIter(file)
+            return CsvIter(file)
             #return iter(reader)
         except FileExistsError:
             return iter(())
@@ -54,10 +55,48 @@ class PgSource:
         self.tables = config.pg_source.tables
 
     def get_tables(self):
-        pass
+        return self.tables
 
 
-class SmartIter:
+    def get_schema(self, table):
+        result = {
+            'name': table
+        }
+        with psycopg2.connect(config.pg_source.conn_string) as conn:
+            cur = conn.cursor()
+
+            #get table key column
+            cur.execute('''
+                        SELECT a.attname, format_type(a.atttypid, a.atttypmod) AS data_type
+                        FROM   pg_index i
+                        JOIN   pg_attribute a ON a.attrelid = i.indrelid
+                                             AND a.attnum = ANY(i.indkey)
+                        WHERE  i.indrelid = %s::regclass
+                        AND    i.indisprimary
+                    ''', (table,))
+            key_column = cur.fetchone()
+            if key_column: result['primary'] =  key_column[0]
+
+            #get columns and types
+            columns = []
+            cur.execute('SELECT * FROM %s LIMIT 1' % table.split(' ')[0])
+            for column in cur.description:
+                col = {}
+                col['name'] = column.name
+                col['type'] = pg_types[column.type_code]['vc_type']
+                columns.append(col)
+        result['columns'] = columns
+        return result
+
+    def get_data_iterator(self, table):
+        with psycopg2.connect(config.pg_source.conn_string) as conn:
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM %s' % table.split(' ')[0])
+            #return iter(cur.fetchall())
+            return PgIter(cur)
+
+
+class CsvIter:
     def __init__(self, file):
         self.file = file
         self.iterator = iter(csv.reader(self.file))
@@ -68,3 +107,19 @@ class SmartIter:
         except StopIteration:
             self.file.close()
         if self.file.closed: raise StopIteration
+
+class PgIter:
+    def __init__(self, cursor):
+        self.cursor = cursor
+
+    def __next__(self):
+        nextRow = self.cursor.fetchone()
+        if not nextRow: raise StopIteration
+        return list(map(lambda x: x.isoformat() if isinstance(x, datetime.date) else x, nextRow))
+
+
+if __name__ == '__main__':
+    pg_source = PgSource()
+    a = pg_source.get_tables()
+    print(a)
+    print(pg_source.get_schema(a[0]))
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from dataloader import DataLoader
-from dataloader import CsvSource
+from data_sources import CsvSource, PgSource
 
 def load_from_csv():
     dataLoader = DataLoader()
     dataLoader.load_metadata()
     dataLoader.load_data(CsvSource())
 
+def load_from_pg():
+    dataLoader = DataLoader()
+    #dataLoader.load_metadata()
+    dataLoader.load_data(PgSource())
+
+
 if __name__ == '__main__':
-    load_from_csv()
+    load_from_pg()
 
