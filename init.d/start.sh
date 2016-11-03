6983533be22db640f09dedcee3874a272a35d836
siem <siemarell@gmail.com>
2016-11-03 12:39:32 +0300

    start fix

--- init.d/start.sh
+++ init.d/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+ETL_PATH=/home/siem/PycharmProjects/nspk
 
 export LANG=en_US.UTF-8
-cd ../
-screen -AmdS etl sudo -u siem python3 main.py
+cd $ETL_PATH
+source venv/bin/activate
+screen -AmdS etl_process sudo -u siem python3 main.py
 
